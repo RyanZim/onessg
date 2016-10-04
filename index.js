3221c75c3d04e663286c818656da9e5a0b76b174
RyanZim <opensrc@ryanzim.com>
2016-10-04 09:59:19 -0400

    Port entire library to promises internally

--- index.js
+++ index.js
@@ -1,78 +1,89 @@
 'use strict';
+var p = require('thenify');
 var fs = require('fs-extra');
 var path = require('path-extra');
 var globby = require('globby');
 var matter = require('gray-matter');
 var cons = require('consolidate');
 var yaml = require('js-yaml');
-var marked = require('marked');
+var marked = p(require('marked'));
 var _ = require('lodash');
-// Directory vars:
+// Config vars:
+var engine;
 var src;
 var layouts;
 var dist;
 module.exports = function (engine, dirs, cb) {
-  // Make dirs available globally
-  setDirs(dirs, function (err) {
-    if (err) cb(err);
-  });
-  // Check that engine is a string:
-  if (typeof engine !== 'string' || engine === '') return cb(new Error('Please pass a valid engine parameter'));
-  // Check that engine is supported by consolidate.js:
-  if (typeof cons[engine] !== 'function') return cb(new Error(`${engine} is not a valid consolidate.js template engine`));
-  // For each file in src:
-  forGlob('**/*.@(html|md|markdown)', function (filePath, cb) {
-    // Load and parse FM:
-    loadFile(filePath, function (err, file) {
-      if (err) return cb(err);
-      // Run through middleware:
-      middleware(file.content, path.extname(filePath), function (err, body) {
-        if (err) return cb(err);
-        // Overwrite body:
-        file.content=body;
-        // Render it:
-        render(file, engine, filePath, function (err, html) {
-          if (err) return cb(err);
-          // Write to file and pass cb
-          writeFile(html, filePath, cb);
-        });
-      });
-    });
-  }, function (err) {
-    cb(err);
-  });
+  // Make engine and dirs available globally:
+  setConf(engine, dirs)
+  .then(function () {
+    // Get files:
+    return globby('**/*.@(html|md|markdown)', {nodir: true, cwd: src});
+  })
+  .then(function (arr) {
+    // Process each file
+    // return a promise when all files have been processed:
+    return Promise.all(arr.map(function (item) {
+      return processFile(item);
+    }));
+  })
+  .then(function () {
+    // Call cb:
+    cb();
+  })
+  .catch(cb);
 };
-// HELPER FUNCTIONS
-function render(file, engine, filePath, cb) {
-  // Get defaults:
-  getDefaults(path.join(src, filePath), function (err, defaults) {
-    if (err) return cb(err);
-    // Set Defaults (cache is set to true by default):
-    _.defaultsDeep(file.data, defaults, {cache: true});
-    // If layout, render:
-    if (file.data._layout) {
-      // Get layouts/layoutName.* :
-      let layout=globby.sync(path.join(layouts, file.data._layout)+'.*')[0];
-      // Globby doesn't throw an error if the layout path doesn't exist, so we do:
-      if (!layout) cb(new Error(`The layout: ${file.data._layout} cannot be found in ${layouts}`));
-      let locals=file.data;
-      locals._body=file.content;
-      // Render with consolidate.js:
-      cons[engine](layout, locals, cb);
-    } else cb(null, file.content); // Else, return body
+
+// Accepts filePath
+// Returns Promise when file has been written
+function processFile(filePath) {
+  // Load file and convert to a data object:
+  return loadFile(filePath)
+  .then(function (data) {
+    // Run through middleware:
+    return middleware(data, path.extname(filePath));
+  })
+  .then(function (data) {
+    return getDefaults(filePath)
+    .then(function (defaults) {
+      // Mutate data & set cache: true :
+      _.defaultsDeep(data, defaults, {cache: true});
+      return data;
+    });
+  })
+  .then(function (data) {
+    // If _layout, render it:
+    if (data._layout) return render(data, filePath);
+    // Else, return _body:
+    else return data._body;
+  })
+  .then(function (html) {
+    // Get path to write to using path-extra:
+    var writePath=path.replaceExt(path.join(dist, filePath), '.html');
+    // Output using fs-extra:
+    return p(fs.outputFile)(writePath, html);
   });
 }
-function getDefaults(filePath, cb) {
-  var dirPath=path.dirname(filePath);
+// HELPER FUNCTIONS
+function render(data) {
+  return globby(path.join(layouts, data._layout)+'.*')
+  .then(function (arr) {
+    var layout = arr[0];
+    // Globby doesn't throw an error if the layout path doesn't exist, so we do:
+    if (!layout) throw new Error(`The layout: ${data._layout} cannot be found in ${layouts}`);
+    // Render with consolidate.js:
+    return p(cons[engine])(layout, data);
+  });
+}
+function getDefaults(filePath) {
+  // Must have src/ in the path so we know when to stop:
+  var dirPath=path.join(src, path.dirname(filePath));
   var arr=[load(dirPath)];
   recurse(dirPath);
-  Promise.all(arr)
+  return Promise.all(arr)
   .then(function (arr) {
     // Combine defaults:
-    cb(null, _.spread(_.defaultsDeep)(arr));
-  })
-  .catch(function (err) {
-    cb(err);
+    return _.spread(_.defaultsDeep)(arr);
   });
   function recurse(dirPath) {
     // If we have reached src/, stop:
@@ -100,69 +111,56 @@ function getDefaults(filePath, cb) {
     });
   }
 }
-function middleware(text, ext, cb) {
+function middleware(data, ext) {
   // Check path's ext:
   switch (ext) {
   case '.html':
     // noop:
-    return cb(null, text);
+    return Promise.resolve(data);
   case '.md':
   case '.markdown':
     // Render markdown:
-    return marked(text, cb);
-  }
-}
-function writeFile(html, filePath, cb) {
-  // Get path to write to using path-extra:
-  var writePath=path.replaceExt(path.join(dist, filePath), '.html');
-  // Output using fs-extra:
-  fs.outputFile(writePath, html, function (err) {
-    cb(err);
-  });
-}
-// loadFile() calls (err, front-matter object)
-function loadFile(name, cb) {
-  fs.readFile(path.join(src, name), 'utf8', function (err, res) {
-    if (err) return cb(err);
-    var json;
-    // Use try...catch for sync front-matter:
-    try {
-      json=matter(res);
-    } catch (e) {
-      return cb(e);
-    }
-    cb(null, json);
-  });
-}
-// Runs iter over each match of pattern and call cb
-function forGlob(pattern, iter, cb) {
-  var arr;
-  try {
-    // Sync doesn't matter here:
-    arr = globby.sync(pattern, {nodir: true, cwd: src});
-  } catch (e) {
-    cb(e);
-  }
-  var count=0;
-  arr.forEach(function (item) {
-    iter(item, function (err) {
-      if (err) return cb(err);
-      if (++count === arr.length) cb(null);
+    return marked(data._body)
+    .then(function (res) {
+      // Overwrite data._body:
+      data._body = res;
+      return data;
     });
+  }
+}
+// Accepts filename
+// Returns Promise(data object)
+function loadFile(name) {
+  return p(fs.readFile)(path.join(src, name), 'utf8')
+  .then(function (text) {
+    return matter(text);
+  })
+  .then(function (file) {
+    var data = file.data;
+    data._body = file.content;
+    return data;
   });
 }
 
-function setDirs(dirs, cb) {
-  // Check that src exists:
-  fs.access(dirs.src, function (err) {
-    if (err) return cb(err);
+function setConf(eng, dirs) {
+  var access = p(fs.access);
+  // Check that src & layouts exists:
+  return Promise.all([
+    access(dirs.src),
+    access(dirs.layouts),
+    new Promise(function (resolve) {
+      // Check that engine is a string:
+      if (typeof eng !== 'string' || eng === '') throw new Error('Please pass a valid engine parameter');
+      // Check that engine is supported by consolidate.js:
+      if (typeof cons[eng] !== 'function') throw new Error(`${eng} is not a valid consolidate.js template engine`);
+      resolve();
+    }),
+  ])
+  .then(function () {
+    // Set vars:
+    src=dirs.src;
+    dist=dirs.dist;
+    layouts=dirs.layouts;
+    engine=eng;
   });
-  // Check that layouts exists:
-  fs.access(dirs.layouts, function (err) {
-    if (err) return cb(err);
-  });
-  src=dirs.src;
-  dist=dirs.dist;
-  layouts=dirs.layouts;
-  cb();
 }
